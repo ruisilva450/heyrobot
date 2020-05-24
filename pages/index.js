8b2099ec4e6fdaacea553528a414564d67d360f0
Rui Silva <rui.silva450@gmail.com>
2020-05-24 21:10:31 +0200

    Added pwa support with temporary favicons

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Card from "../components/card";
+import Head from "next/head";
 import { useState, useEffect } from "react";
 const words = require("../words.json");
 
@@ -18,6 +19,38 @@ export default () => {
   useEffect(() => {});
   return (
     <div>
+      <Head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta
+          name="description"
+          content="Hey Robot is an application to play with your digital assistant"
+        />
+        <title>Hey Robot</title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a65c83" />
+        <meta name="msapplication-TileColor" content="#feff5f" />
+        <meta name="theme-color" content="#feff5f" />
+      </Head>
       <div className="scoring">
         <div
           className={`score
